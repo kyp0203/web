46d11f9393bd8f11484178c74aa9af4082d127a3
kyp0203 <kyuyeonpark0203@gmail.com>
2024-05-17 22:53:14 +0900

    20240517

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ let obj = {name:"철수", age:"20"};
 for(let key in obj){
 	console.log(key + ": " + obj[key]);
 }
+
+let arr = ["orange", "banana", "apple"];
+for(let index in arr){
+	console.log(index + ": " + arr[index]);
+}
+
+const gugudan = () => {
+    for(let i =1; i<=9; i++){
+        console.log(`3*${i} = ${3*i}`);
+    }
+};
+gugudan();

5f8bc014d5dc92087473b9e921c277641026be2d
kyp0203 <kyuyeonpark0203@gmail.com>
2024-05-18 21:23:12 +0900

    20240518(2)

--- script.js
+++ script.js
@@ -27,9 +27,60 @@ sum1(10,20);
 // sum(10,20);
 // console.log("out: " + result); //ReferenceError: result is not defined
 
-function sum(num1, num2){
+function sum2(num1, num2){
     let result = num1 + num2;
     return result;
 }
-const result = sum(10,20);
+const result = sum2(10,20);
 console.log("out: " + result); 
+
+let a = 10; //전역 스코프
+function sum3(){
+	console.log(`함수 내부: ${a}`);
+}
+sum3();
+console.log(`함수 외부: ${a}`);
+
+// var printHello;
+// printHello();
+// printHello = function printHello(){
+// 	console.log("Hello");
+// }
+
+// var text = "outside";
+// function printScope(){
+//     console.log(text);
+//     var text = "inside";
+// };
+// printScope();
+
+(function init(){
+    console.log("initialized!");
+})(); 
+// init();
+
+console.log("----------------------------");
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    str = input[0];
+
+
+for (let i =0; i< str.length; i++){
+    if(str[i] === str[i].toUpperCase()){
+        input.push(str[i].toLowerCase());
+    }else{
+        input.push(str[i].toUpperCase());
+    }
+}
+    
+});

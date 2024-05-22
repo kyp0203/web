d5fcf0665335cd8211accc2341c6584df0c9b908
kyp0203 <kyuyeonpark0203@gmail.com>
2024-05-22 20:56:32 +0900

    240522

--- script.js
+++ script.js
@@ -61,26 +61,59 @@ console.log(`함수 외부: ${a}`);
 
 console.log("----------------------------");
 
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
 
-let input = [];
+// let input = [];
 
-rl.on('line', function (line) {
-    input = [line];
-}).on('close',function(){
-    str = input[0];
+// rl.on('line', function (line) {
+//     input = [line];
+// }).on('close',function(){
+//     str = input[0];
 
 
-for (let i =0; i< str.length; i++){
-    if(str[i] === str[i].toUpperCase()){
-        input.push(str[i].toLowerCase());
-    }else{
-        input.push(str[i].toUpperCase());
-    }
-}
+// for (let i =0; i< str.length; i++){
+//     if(str[i] === str[i].toUpperCase()){
+//         input.push(str[i].toLowerCase());
+//     }else{
+//         input.push(str[i].toUpperCase());
+//     }
+// }
     
-});
+// });
+
+console.log("----------------------------");
+
+let num=10;
+let copyNum = num;
+num=20;
+console.log(num);   //20
+console.log(copyNum);   //10 
+//변수 num만 값이 변경되고 변수 copyNum은 변경되지 않음
+//변수 copyNum은 변수 num의 값을 복사한 별도의 데이터를 가지고 있기 때문
+//즉 두 데이터는 연동되어 있지 않음
+
+console.log("----------------------------");
+
+const person = {
+    name: "kyeon"
+};
+const copyPerson = person;
+person.name = "KYU";
+console.log(person.name);   //KYU
+console.log(copyPerson.name);   //KYU
+//person에 할당된 객체의 속성값을 변경했는데 변수 copyPerson에 복사된 객체의 속성값까지 변경됨
+//객체가 변수에 할당될 때 변수는 주소 값만 가지고 있어서 복사할 때도 주소 값만 복사하기 때문
+//그래서 두 변수는 같은 주소 값을 가지게 되고 바라보는 원본 데이터의 위치도 같음
+
+const random = Math.random();
+console.log(random);
+
+function getMaxRandom(max){
+    return Math.floor(Math.random()*max)+1;
+}
+const maxRandom = getMaxRandom(20);
+console.log(maxRandom); //0이상 20 이하의 무작위 정수

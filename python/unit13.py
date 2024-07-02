5d61d3bc53469745943a59ed4d5493aac7dbb156
kyp0203 <kyuyeonpark0203@gmail.com>
2024-07-02 13:19:13 +0900

    240701 python 시작

---
+++ python/unit13.py
@@ -0,0 +1,16 @@
+# 연습문제
+x = 5
+if x != 10:
+    print('ok')
+    
+# 심사문제
+a = int(input()) #a를 정수형으로 변환
+b = input()
+
+if b == 'Cash3000':
+    print(a-3000)
+    
+    if b == 'Cash5000':
+        print(a-5000)
+    
+
---
+++ python/unit4.py
@@ -0,0 +1,7 @@
+# 연습문제
+print('Hello World!')
+print('Python Programming')
+
+# 심사문제
+print('Hello world!')
+print('Hello world!')

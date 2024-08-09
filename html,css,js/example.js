35fc9874dedc5477fc58984cd4bb3bec080eb621
kyp0203 <kyuyeonpark0203@gmail.com>
2024-08-09 17:07:54 +0900

    0809

---
+++ html,css,js/example.js
@@ -0,0 +1,27 @@
+
+
+//null - 어떤 값이 의도적으로 비어있음을 의미함
+let empty = null;
+
+console.log(empty);
+
+// String - 따옴표를 사용함
+
+let name = "Kyeon"
+let email = 'lucy0328@naver.com'
+let hello = `Hello ${name}?!`
+
+console.log(name);
+console.log(email);
+console.log(hello);
+
+//Object(객체 데이터) - 여러 데이터를 Key:Value 형태로 저장함 { }
+let user = {
+	id: 'kyeon',
+	age: 23,
+	isValid: true
+};
+
+console.log(user.id);
+console.log(user.age);
+console.log(user.isValid);
